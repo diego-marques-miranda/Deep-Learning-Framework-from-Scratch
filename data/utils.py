8423d29cff8d7b19ebffae1f82942ed7c7b09569
diego-marques-miranda <diegomm.pu@gmail.com>
2026-08-24 09:20:00 -0300

    feat(data): add optional rng parameter to train_test_split

--- data/utils.py
+++ data/utils.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def train_test_split(X, y, test_size):
+def train_test_split(X, y, test_size, rng=np.random.default_rng()):
     """Splits arrays or matrices into random train and validation/test subsets using index shuffling."""
     indexes = np.arange(X.shape[0])
-    np.random.shuffle(indexes)
+    rng.shuffle(indexes)
     split_idx = int(X.shape[0] * (1 - test_size))
     
     train_idx, val_idx = indexes[:split_idx], indexes[split_idx:]
